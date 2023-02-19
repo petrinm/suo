4953a24706ec5def5971f06b66e317cfe4151e35
petrinm <petrinm@gmail.com>
2023-02-19 23:36:50 +0200

    Use std::popcount and suo::bit_parity in golay24

--- libsuo/coding/golay24.cpp
+++ libsuo/coding/golay24.cpp
@@ -13,29 +13,11 @@
  R.H. Morelos-Zaragoza, The Art of Error Correcting Coding, Wiley, 2002; Section 2.2.3
 */
 
-#include <stdint.h>
-
-
-/****** START OF SUO MODIFICATION *********/
-// Remove the volk dependency to make the code easier to compile.
-//#include <volk/volk.h>
-#define volk_32u_popcnt(ret, val)  *(ret) = __builtin_popcountl(val)
-/******* END OF SUO MODIFICATIONS *********/
-
-
-
 #include "golay24.hpp"
 
+#include <bit> // std::popcount
+#include "framing/utils.hpp" // suo::bit_parity
 
-// Replacement for __builtin_parity in compilers other than GCC and clang
-#if !defined(__GNUC__) && !defined(__clang__)
-static inline uint32_t __builtin_parity(uint32_t x)
-{
-    uint32_t p;
-    volk_32u_popcnt(&p, x);
-    return p & 1;
-}
-#endif /* !defined(__GNUC__) && !defined(__clang__) */
 
 #define N 12
 
@@ -53,7 +35,7 @@ int encode_golay24(uint32_t* data)
     s = 0;
     for (i = 0; i < N; i++) {
         s <<= 1;
-        s |= __builtin_parity(H[i] & r);
+        s |= suo::bit_parity(H[i] & r);
     }
 
     *data = ((0xFFF & s) << N) | r;
@@ -67,18 +49,18 @@ int decode_golay24(uint32_t* data)
     uint16_t q; /* modified syndrome */
     uint32_t e; /* estimated error vector */
     int i;
-    uint32_t popcount;
+    uint32_t count;
 
     // Step 1. s = H*r
     s = 0;
     for (i = 0; i < N; i++) {
         s <<= 1;
-        s |= __builtin_parity(H[i] & r);
+        s |= suo::bit_parity(H[i] & r);
     }
 
     // Step 2. if w(s) <= 3, then e = (s, 0) and go to step 8
-    volk_32u_popcnt(&popcount, s);
-    if (popcount <= 3) {
+    count = std::popcount(s);
+    if (count <= 3) {
         e = s;
         e <<= N;
         goto step8;
@@ -86,8 +68,8 @@ int decode_golay24(uint32_t* data)
 
     // Step 3. if w(s + B[i]) <= 2, then e = (s + B[i], e_{i+1}) and go to step 8
     for (i = 0; i < N; i++) {
-        volk_32u_popcnt(&popcount, s ^ B(i));
-        if (popcount <= 2) {
+        count = std::popcount(s ^ B(i));
+        if (count <= 2) {
             e = s ^ B(i);
             e <<= N;
             e |= 1 << (N - i - 1);
@@ -99,20 +81,20 @@ int decode_golay24(uint32_t* data)
     q = 0;
     for (i = 0; i < N; i++) {
         q <<= 1;
-        q |= __builtin_parity(B(i) & s);
+        q |= suo::bit_parity(B(i) & s);
     }
 
     // Step 5. If w(q) <= 3, then e = (0, q) and go to step 8
-    volk_32u_popcnt(&popcount, q);
-    if (popcount <= 3) {
+    count = std::popcount(q);
+    if (count <= 3) {
         e = q;
         goto step8;
     }
 
     // Step 6. If w(q + B[i]) <= 2, then e = (e_{i+1}, q + B[i]) and got to step 8
     for (i = 0; i < N; i++) {
-        volk_32u_popcnt(&popcount, q ^ B(i));
-        if (popcount <= 2) {
+        count = std::popcount(q ^ B(i));
+        if (count <= 2) {
             e = 1 << (2 * N - i - 1);
             e |= q ^ B(i);
             goto step8;
@@ -126,6 +108,6 @@ step8:
     // Step 8. c = r + e
     *data = r ^ e;
 
-    volk_32u_popcnt(&popcount, e);
-    return popcount;
+    count = std::popcount(e);
+    return count;
 }
