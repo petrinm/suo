6ccb25004741afba61323cf0bcd9b1551b1e541d
petrinm <petrinm@gmail.com>
2023-02-20 15:49:30 +0200

    Fixing GMSK demod

--- libsuo/modem/demod_gmsk_cont.cpp
+++ libsuo/modem/demod_gmsk_cont.cpp
@@ -5,9 +5,7 @@
 
 #include "modem/demod_gmsk_cont.hpp"
 #include "registry.hpp"
-#include "plotter.hpp"
 
-#include <matplot/matplot.h>
 
 using namespace std;
 using namespace suo;
@@ -23,16 +21,16 @@ GMSKContinousDemodulator::Config::Config() {
 	frequency_offset = 0.0f;
 	samples_per_symbol = 4;
 	filter_delay = 5;
-	bt = 0.3;
+	bt = 0.5;
 
-	pll_bandwidth0 = 0.06f;
-	pll_bandwidth1 = 0.005f;
+	pll_bandwidth0 = 4e-7f;
+	pll_bandwidth1 = 4e-7f;
 
-	agc_bandwidth0 = 0.001f;
-	agc_bandwidth1 = 0.001f;
+	agc_bandwidth0 = 9e-2f;
+	agc_bandwidth1 = 2e-2f;
 
-	symsync_bandwidth0 = 0.1f;
-	symsync_bandwidth1 = 0.1f;
+	symsync_bandwidth0 = 0.01f; // 0.01f;
+	symsync_bandwidth1 = 0.006f;
 
 }
 
@@ -42,14 +40,9 @@ GMSKContinousDemodulator::GMSKContinousDemodulator(const Config& conf) :
 
 	/* Configure a resampler for a fixed oversampling ratio */
 	float resamprate = conf.symbol_rate * conf.samples_per_symbol / conf.sample_rate;
-	double bw = 0.4 * resamprate / conf.samples_per_symbol;
+	double bw = 0.75 * resamprate / conf.samples_per_symbol;
 	int semilen = lroundf(1.0f / bw);
-	unsigned int npfb = 32;
-	cerr << "resamprate: " << resamprate << endl;
-	cerr << "bw: " << bw << endl;
-	cerr << "semilen: " << semilen << endl;
-
-	l_resamp = resamp_crcf_create(resamprate, semilen, bw, 60.0f, npfb);
+	l_resamp = resamp_crcf_create(resamprate, semilen, bw, 60.0f, 16);
 
 	/* Calculate maximum number of output samples after feeding one sample
 	 * to the resampler. This is needed to allocate a big enough array. */
@@ -70,41 +63,24 @@ GMSKContinousDemodulator::GMSKContinousDemodulator(const Config& conf) :
 	 * AGC: Automatic gain control
 	 */
 	l_agc = agc_crcf_create();
-	agc_crcf_set_bandwidth(l_agc, 5e-3f); // TODO: /= conf.samples_per_symbol
+	agc_crcf_set_bandwidth(l_agc, conf.agc_bandwidth0 / conf.samples_per_symbol);
 	//agc_crcf_set_gain_limits(l_agc, 1e-2f, 1e2f);
-
 	l_bg_agc = agc_crcf_create();
-	agc_crcf_set_bandwidth(l_bg_agc, 1e-6f / conf.samples_per_symbol);
+	agc_crcf_set_bandwidth(l_bg_agc, 2e-3f / conf.samples_per_symbol);
 
-
-	l_demod_delay = windowcf_create(4 * conf.samples_per_symbol);
 
 	float kf = 0.01 * conf.samples_per_symbol;
 	k_ref = 1.0f / (2 * M_PI * kf);
 
-	/* Symbol synchroniser */
-	unsigned int m = 5;                 // filter delay (symbols)
-	float        beta = 0.5f;           // filter excess bandwidth factor
-	unsigned int num_filters = 32;      // number of filters in the bank
-	unsigned int num_symbols = 400;     // number of data symbols
+	/* 
+	 * Symbol synchronizer:
+	 * - Undistors the gaussian filtering
+	 * - Runs polyphase filter bank for symbol timing recovery 
+	 * - Decimates to symbol rate
+	 */
+	l_symsync = symsync_crcf_create_rnyquist(LIQUID_FIRFILT_GMSKRX, conf.samples_per_symbol, conf.filter_delay, conf.bt, 32);
+	symsync_crcf_set_lf_bw(l_symsync, conf.symsync_bandwidth0 / conf.samples_per_symbol);
 
-
-	//l_symsync = symsync_rrrf_create_rnyquist(LIQUID_FIRFILT_ARKAISER, conf.samples_per_symbol, m, beta, num_filters);
-	l_symsync = symsync_rrrf_create_kaiser(conf.samples_per_symbol, 8, 0 /* ignored */, 64);
-	symsync_rrrf_set_lf_bw(l_symsync, conf.symsync_bandwidth0 / conf.samples_per_symbol);
-
-	//symsync_rrrf_set_output_rate(l_symsync, conf.samples_per_symbol); // Disable decimation
-
-#if 0
-	/* Compute filter coefficients */
-	size_t h_len = 2 * conf.samples_per_symbol * conf.filter_delay + 1;
-	float h[h_len];
-	liquid_firdes_gmskrx(conf.samples_per_symbol, conf.filter_delay, conf.bt, 0.0f, h);
-	l_filter = firfilt_rrrf_create(h, h_len);
-#else
-	l_filter = firfilt_rrrf_create_rnyquist(LIQUID_FIRFILT_GMSKRX, conf.samples_per_symbol, 8, conf.bt, 0);
-	firfilt_rrrf_set_scale(l_filter, 1.f / (float)conf.samples_per_symbol);
-#endif
 
 	reset();
 }
@@ -113,15 +89,12 @@ GMSKContinousDemodulator::GMSKContinousDemodulator(const Config& conf) :
 GMSKContinousDemodulator::~GMSKContinousDemodulator()
 {
 	agc_crcf_destroy(l_agc);
-	firfilt_rrrf_destroy(l_filter);
-	symsync_rrrf_destroy(l_symsync);
-	windowcf_destroy(l_demod_delay);
+	symsync_crcf_destroy(l_symsync);
 }
 
 
 void GMSKContinousDemodulator::reset() {
 	x_prime = 0.0f;
-	//filter_rrrf_reset(l_filter);
 	nco_crcf_set_frequency(l_nco, nco_1Hz * (conf.center_frequency + conf.frequency_offset));
 	receiver_lock = false;
 }
@@ -161,28 +134,10 @@ void GMSKContinousDemodulator::sinkSamples(const SampleVector& samples, Timestam
 	Sample null;
 
 
-	Plotter plot_a(200);
-	Plotter plot_b(200);
-	ComplexPlotter iq_plot1(1024);
-	iq_plot1.modulo = conf.samples_per_symbol;
-	iq_plot1.offset = 0;
-
-	ComplexPlotter iq_plot2(200);
-	iq_plot2.modulo = conf.samples_per_symbol;
-	iq_plot2.offset = 0;
-	iq_plot2.auto_clear = true;
-
-
 
 	for (size_t si = 0; si < samples.size(); si++) {
 		unsigned nsamp2 = 0, si2;
 		Sample s = samples[si];
-
-		if (0 && iq_plot1.push(s)) {
-			iq_plot1.plot_fft();
-			matplot::show();
-			return;
-		}
 
 		/* Downconvert and resample one input sample at a time */
 		nco_crcf_step(l_nco);
@@ -194,18 +149,26 @@ void GMSKContinousDemodulator::sinkSamples(const SampleVector& samples, Timestam
 		for(si2 = 0; si2 < nsamp2; si2++) {
 			Sample s2 = samples2[si2];
 
-			/* Update AGC */
+			/* Update AGC just to track RSSI's */
 			agc_crcf_execute(l_bg_agc, s2, &null);
 			agc_crcf_execute(l_agc, s2, &null);
-			//agc_crcf_execute(l_agc, s2, &s2);
 
 #if 1
-			/* Update PLL feedback (Costas loop for QPSK) */
-			float phase_error = copysign(s2.imag(), s2.real()) - copysign(s2.real(), s2.imag());
+			/*  */
+			float phase_error = arg(conj(x_primee) * s2) * k_ref;
+			x_primee = s2;
+			nco_crcf_mix_up(l_nco, x_primee, &x_primee);
+			nco_crcf_pll_step(l_nco, -100000 * phase_error);
+			nco_crcf_mix_down(l_nco, x_primee, &x_primee);
 #else
-			float phase_error = s2.real() * s2.imag();
+			/* Update PLL feedback (Costas loop for QPSK) */
+			//float phase_error = copysign(s2.imag(), s2.real()) - copysign(s2.real(), s2.imag());
+			//float phase_error = s2.real() * s2.imag();
+			float phase_error = (s2 * conj(x_primee)).imag();
+			x_primee = s2;
+			nco_crcf_pll_step(l_nco, -100* phase_error);
 #endif
-			nco_crcf_pll_step(l_nco, phase_error);
+
 
 			/* Clamp the PLL frequency between min and max */
 			float freq = nco_crcf_get_frequency(l_nco);
@@ -215,71 +178,26 @@ void GMSKContinousDemodulator::sinkSamples(const SampleVector& samples, Timestam
 				nco_crcf_set_frequency(l_nco, freq_min);
 
 
-			if (receiver_lock) {
-				if (0 && plot_a.push(freq / nco_1Hz)) {
-					plot_a.plot();
-					matplot::show();
-					return;
-				}
-				if (0 && iq_plot2.push(s2)) {
-					iq_plot2.plot_constellation();
-					matplot::show();
-					return;
-				}
-			}
-
-			/* Quadrature FM-demodulation */
-			float phi = arg(conj(x_prime) * s2) * k_ref;
-			x_prime = s2;
-
-			/* */
-			firfilt_rrrf_push(l_filter, phi);
-			firfilt_rrrf_execute(l_filter, &phi);
-			if (0 && iq_plot2.push(s2)) {
-				iq_plot2.plot_constellation();
-				matplot::show();
-				return;
-			}
-
-			/* Run symbols sync */
-			float synced_symbols[4];
+			/* Run symbols synchronization.
+			 * This has also gaussian undistortion and decimation */
+			Complex synced_symbol;
 			unsigned int output_symbols = 0;
-			symsync_rrrf_execute(l_symsync, &phi, 1, synced_symbols, &output_symbols);
-
-#if 0
-			for (size_t j = 0; j < output_symbols; j++) {
-				plot2.push_back(synced_symbols[j]);
-				if (plot2.size() == plot2.capacity()) {
-					matplot::figure();
-					matplot::hold(matplot::on);
-					matplot::plot(plot1);
-					matplot::plot(plot2);
-					matplot::show();
-					return;
-				}
-			}
-#endif
-
-			assert(output_symbols <= 1);
+			symsync_crcf_execute(l_symsync, &s2, 1, &synced_symbol, &output_symbols);
 			if (output_symbols == 0)
 				continue;
 
+			assert(output_symbols == 1);
 
-			float synced_symbol = synced_symbols[0];
-			//cout << "synced_symbol " << synced_symbol << endl;
-#if 0
-			if (1 && plot_b.push(synced_symbol)) {
-				plot_b.plot();
-				matplot::show();
-				return;
-			}
-#endif
+			/* Quadrature FM-demodulation */
+			float phi = arg(conj(x_prime) * synced_symbol) * k_ref;
+			x_prime = synced_symbol;
 
+		
 			/* Process one output symbol from synchronizer */
 			Timestamp symbol_time = now + si * sample_ns;
 			//now -= conf.filter_delay * resampint; // or / resamprate
 
-			Symbol decision = (synced_symbol >= 0) ? 1 : 0;
+			Symbol decision = (phi >= 0) ? 1 : 0;
 			//cout << (int)decision << " ";
 			sinkSymbol.emit(decision, symbol_time);
 
@@ -295,16 +213,7 @@ void GMSKContinousDemodulator::sinkSamples(const SampleVector& samples, Timestam
 				/* No sync, no lock */
 				receiver_lock = false;
 			}
-			
 #endif
-
-
-			/*
-			frame->setMetadata("cfo", nco_crcf_get_frequency(l_nco) / nco_1Hz);
-			frame->setMetadata("rssi", agc_crcf_get_rssi(l_agc));
-			frame->setMetadata("bg_rssi", agc_crcf_get_rssi(l_bg_agc));
-			*/
-
 
 		}
 	}
@@ -313,19 +222,23 @@ void GMSKContinousDemodulator::sinkSamples(const SampleVector& samples, Timestam
 void GMSKContinousDemodulator::lockReceiver(bool locked, Timestamp now) {
 	receiver_lock = locked;
 	if (locked) {
+		
+		setMetadata.emit("cfo", nco_crcf_get_frequency(l_nco) / nco_1Hz);
+		setMetadata.emit("rssi", agc_crcf_get_rssi(l_agc));
+		setMetadata.emit("bg_rssi", agc_crcf_get_rssi(l_bg_agc));
 
-		cout << "cfo: " << (nco_crcf_get_frequency(l_nco) / nco_1Hz) << endl;
-		cout << "rssi: " << agc_crcf_get_rssi(l_agc) << endl;
-		cout << "bg_rssi: " << agc_crcf_get_rssi(l_bg_agc) << endl;
-
-		symsync_rrrf_set_lf_bw(l_symsync, conf.symsync_bandwidth1 / conf.samples_per_symbol);
-		agc_crcf_set_bandwidth(l_agc, conf.agc_bandwidth1);
+		symsync_crcf_set_lf_bw(l_symsync, conf.symsync_bandwidth1 / conf.samples_per_symbol);
+		//symsync_rrrf_set_lf_bw(l_symsync, conf.symsync_bandwidth1 / conf.samples_per_symbol);
+		nco_crcf_pll_set_bandwidth(l_nco, conf.pll_bandwidth1 * nco_1Hz);
+		agc_crcf_set_bandwidth(l_agc, conf.agc_bandwidth1 / conf.samples_per_symbol);
 		agc_crcf_lock(l_bg_agc);
 	}
 	else {
-		symsync_rrrf_set_lf_bw(l_symsync, conf.symsync_bandwidth0 / conf.samples_per_symbol);
-		agc_crcf_set_bandwidth(l_agc, conf.agc_bandwidth0);
-		agc_crcf_lock(l_bg_agc);
+		symsync_crcf_set_lf_bw(l_symsync, conf.symsync_bandwidth0 / conf.samples_per_symbol);
+		//symsync_rrrf_set_lf_bw(l_symsync, conf.symsync_bandwidth0 / conf.samples_per_symbol);
+		nco_crcf_pll_set_bandwidth(l_nco, conf.pll_bandwidth0 * nco_1Hz);
+		agc_crcf_set_bandwidth(l_agc, conf.agc_bandwidth0 / conf.samples_per_symbol);
+		agc_crcf_unlock(l_bg_agc);
 	}
 }
 
--- libsuo/modem/demod_gmsk_cont.hpp
+++ libsuo/modem/demod_gmsk_cont.hpp
@@ -2,7 +2,7 @@
 
 #include "suo.hpp"
 #include <liquid/liquid.h>
-
+#include "plotter.hpp"
 
 namespace suo {
 
@@ -62,6 +62,10 @@ public:
 	GMSKContinousDemodulator(const Config& conf = Config());
 	~GMSKContinousDemodulator();
 
+	GMSKContinousDemodulator(const GMSKContinousDemodulator&) = delete;
+	GMSKContinousDemodulator& operator=(const GMSKContinousDemodulator&) = delete;
+
+
 	void reset();
 
 	void sinkSamples(const SampleVector& samples, Timestamp timestamp);
@@ -69,7 +73,9 @@ public:
 
 	Port<Symbol, Timestamp> sinkSymbol;
 	Port<SoftSymbol, Timestamp> sinkSoftSymbol;
+	Port<const std::string&, const MetadataValue&> setMetadata;
 
+	void setFrequency(float frequency);
 	void setFrequencyOffset(float frequency_offset);
 
 
@@ -84,23 +90,18 @@ private:
 	Timestamp sample_ns;
 	unsigned resampint;
 	float nco_1Hz;
-	float afc_speed;
 	bool receiver_lock;
 
-	Sample x_prime;
+	Sample x_prime, x_primee;
 	float freq_min, freq_max;
-
 	float k_ref;
 
 	/* liquid-dsp objects */
 	nco_crcf l_nco;
 	resamp_crcf l_resamp;
+	symsync_crcf l_symsync;
 	agc_crcf l_agc;
 	agc_crcf l_bg_agc;
-	firfilt_rrrf l_filter;
-	float k_inv;
-	symsync_rrrf l_symsync;
-	windowcf l_demod_delay;
 
 	/* Buffers */
 	Frame* frame;
